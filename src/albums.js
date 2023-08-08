4bcae39cec51db3e4d0fe4b930cf34cc965b0c8f
Diego Roux <diegoroux04@protonmail.com>
2023-08-08 17:12:44 -0600

    [api]: Finished album endpoint class.
    Signed-off-by: Diego Roux <diegoroux04@protonmail.com>

--- src/albums.js
+++ src/albums.js
@@ -59,4 +59,47 @@ export class SpotifyAlbums {
 
         return await this._get('/albums?' + params);
     }
+
+    /**
+     * Get Spotify catalog information about an album’s tracks.
+     * @param {string} id - The Spotify ID of the album.
+     * @param {string} market - An ISO 3166-1 alpha-2 country code. Only content that is
+     * available in that market will be returned, the country associated with the user account
+     * will take priority over this parameter.
+     * @param {number} limit - The maximum number of items to return. Default: 20.
+     * Minimum: 1.Maximum: 50.
+     * @param {number} offset - The index of the first item to return.
+     * @returns 
+    */
+    async get_tracks(id, market='XX', limit=20, offset=0) {
+        let params = new URLSearchParams({
+            market: market,
+            limit: limit,
+            offset: offset
+        });
+
+        return await this._get('/albums/' + id + '/tracks?' + params);
+    }
+
+    /**
+     * Get a list of new album releases featured in Spotify.
+     * @param {string} country - An ISO 3166-1 alpha-2 country code. Provide this parameter
+     * if you want the list of returned items to be relevant to a particular country. 
+     * @param {number} limit - The maximum number of items to return. Default: 20.
+     * Minimum: 1.Maximum: 50.
+     * @param {number} offset - The index of the first item to return.
+     * @returns {Promise}
+    */
+    async get_new_releases(country='', limit=20, offset=0) {
+        let params = new URLSearchParams({
+            limit: limit,
+            offset: offset
+        });
+
+        if (country !== '') {
+            params.append('country', country);
+        }
+
+        return await this._get('/browse/new-releases?' + params);
+    }
 }
